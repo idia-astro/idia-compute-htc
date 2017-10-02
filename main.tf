c25d538c6cb736616b29c58bad718901b78933e2
Timothy Carr <timothy.carr@uct.ac.za>
2017-10-02 16:48:33 +0200

    Updated main.tf with comments

--- main.tf
+++ main.tf
@@ -6,6 +6,15 @@ resource "openstack_compute_keypair_v2" "htc" {
   public_key = "${file("${var.ssh_key_file}.pub")}" # Path of your SSH key
 }
 
+# Create an Openstack router to route headnode and worker machines
+# onto the internet.
+
+resource "openstack_networking_router_v2" "htc" {
+  name             = "htc"
+  admin_state_up   = "true"
+  external_gateway = "${var.external_gateway}"
+}
+# Create a subnet which workers and headnode will communicate on.
 resource "openstack_networking_subnet_v2" "htc" {
   name            = "htc"
   network_id      = "${openstack_networking_router_v2.htc.id}"
@@ -14,17 +23,13 @@ resource "openstack_networking_subnet_v2" "htc" {
   dns_nameservers = ["8.8.8.8", "8.8.4.4"]
 }
 
-resource "openstack_networking_router_v2" "htc" {
-  name             = "htc"
-  admin_state_up   = "true"
-  external_gateway = "${var.external_gateway}"
-}
-
+# Create an interface on the network router.
 resource "openstack_networking_router_interface_v2" "htc" {
   router_id = "${openstack_networking_router_v2.htc.id}"
   subnet_id = "${openstack_networking_subnet_v2.htc.id}"
 }
 
+# Create a resource for floating ips and attach to the network router.
 resource "openstack_networking_floatingip_v2" "htc" {
   pool       = "${var.pool}"
   depends_on = ["openstack_networking_router_interface_v2.htc"]
@@ -35,6 +40,8 @@ resource "openstack_compute_floatingip_associate_v2" "htc" {
   instance_id = "${openstack_compute_instance_v2.htc.id}"
   depends_on = ["openstack_networking_router_interface_v2.htc"]
 }
+
+# Create the headnode instance so that users are able to login and submit jobs
 
 resource "openstack_compute_instance_v2" "htc" {
   name = "htc"
