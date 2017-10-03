c49b9c25e704823e8100c87f69394043846341f3
Timothy Carr <timothy.carr@uct.ac.za>
2017-10-03 11:41:29 +0200

    Shuffled around the networking resources, updated the image_id to make use of the latest ubuntu 16.04 image if their are multiples with the same name uploaded

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
 # IDIA Execution Framework for DAG using Terraform
 
+# Find most recent Ubuntu 16.04 LTS Image
+data "openstack_images_image_v2" "ubuntu1604" {
+  name = "ubuntu 16.04 LTS amd64"
+  most_recent = true
+}
+
 # Import your SSH public key into OpenStack and give it a name.
 resource "openstack_compute_keypair_v2" "htc" {
   name = "htc-compute_keypair" # SSH key's name
   public_key = "${file("${var.ssh_key_file}.pub")}" # Path of your SSH key
-}
-
-# Create an Openstack Router to route traffic from the headnode and worker machines
-
-resource "openstack_networking_router_v2" "htc" {
-  name             = "htc"
-  admin_state_up   = "true"
-  external_gateway = "${var.external_gateway}"
 }
 
 # Create a network
@@ -27,6 +25,13 @@ resource "openstack_networking_subnet_v2" "htc" {
   cidr            = "10.0.0.0/24"
   ip_version      = 4
   dns_nameservers = ["8.8.8.8", "8.8.4.4"]
+}
+
+# Create an Openstack Router to route traffic from the headnode and worker machines
+resource "openstack_networking_router_v2" "htc" {
+  name             = "htc"
+  admin_state_up   = "true"
+  external_gateway = "${var.external_gateway}"
 }
 
 # Create an interface on the network router.
@@ -51,23 +56,24 @@ resource "openstack_compute_floatingip_associate_v2" "htc" {
 resource "openstack_compute_instance_v2" "htc" {
   name = "htc"
   availability_zone = "uct"
-  image_id = "${var.image}"
+  image_id = "${data.openstack_images_image_v2.ubuntu1604.id}"
   flavor_name = "${var.flavor}"
   key_pair = "${openstack_compute_keypair_v2.htc.name}"
   security_groups = ["default"]
   network {
     name = "htc"
   }
-  provisioner "remote-exec" {
-     connection {
-       user     = "${var.ssh_user_name}"
-       private_key = "${file(var.ssh_key_file)}"
-     }
-     inline = [
-       "sudo apt-get -y update",
-       "sudo apt-get -y upgrade",
-       ]
-     }
+//  provisioner "remote-exec" {
+//     connection {
+//       agent = "true"
+//     user     = "${var.ssh_user_name}"
+//       private_key = "${file(var.ssh_key_file)}"
+//     }
+//     inline = [
+//       "sudo apt-get -y update",
+//       "sudo apt-get -y upgrade",
+//       ]
+//     }
 }
 
 // Create the worker nodes and increase / decrease the count based on the number workers required
@@ -77,7 +83,7 @@ resource "openstack_compute_instance_v2" "workers" {
   name = "${format("htc-worker-%02d", count.index+1)}"
   key_pair = "${openstack_compute_keypair_v2.htc.name}"
   availability_zone = "uct"
-  image_id = "${var.image}"
+  image_id = "${data.openstack_images_image_v2.ubuntu1604.id}"
   flavor_name = "${var.flavor}"
   security_groups = ["default"]
   depends_on = ["openstack_compute_instance_v2.htc"]
