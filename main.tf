1f950ed9f673e720815f3e42f4d1ac6a8d4a0456
Timothy Carr <timothy.carr@uct.ac.za>
2017-10-03 14:21:34 +0200

    Aligned code

--- main.tf
+++ main.tf
@@ -2,20 +2,20 @@
 
 # Find most recent Ubuntu 16.04 LTS Image
 data "openstack_images_image_v2" "ubuntu1604" {
-  name = "ubuntu 16.04 LTS amd64"
+  name        = "ubuntu 16.04 LTS amd64"
   most_recent = true
 }
 
 # Import your SSH public key into OpenStack and give it a name.
 resource "openstack_compute_keypair_v2" "htc" {
-  name = "htc-compute_keypair" # SSH key's name
-  public_key = "${file("${var.ssh_key_file}.pub")}" # Path of your SSH key
+  name        = "htc-compute_keypair" # SSH key's name
+  public_key  = "${file("${var.ssh_key_file}.pub")}" # Path of your SSH key
 }
 
 # Create a network
 resource "openstack_networking_network_v2" "htc" {
-  name           = "htc"
-  admin_state_up = "true"
+  name            = "htc"
+  admin_state_up  = "true"
 }
 
 # Create a subnet which workers and headnode will communicate on.
@@ -36,34 +36,57 @@ resource "openstack_networking_router_v2" "htc" {
 
 # Create an interface on the network router.
 resource "openstack_networking_router_interface_v2" "htc" {
-  router_id = "${openstack_networking_router_v2.htc.id}"
-  subnet_id = "${openstack_networking_subnet_v2.htc.id}"
+  router_id     = "${openstack_networking_router_v2.htc.id}"
+  subnet_id     = "${openstack_networking_subnet_v2.htc.id}"
 }
 
 # Create a resource to retrieve a floating IP from the DHCP Pool
 resource "openstack_networking_floatingip_v2" "htc" {
-  pool       = "${var.pool}"
+  pool           = "${var.pool}"
 }
 
 # We now associate the assigned floating IP to the instance created.
 resource "openstack_compute_floatingip_associate_v2" "htc" {
-  floating_ip = "${openstack_networking_floatingip_v2.htc.address}"
-  instance_id = "${openstack_compute_instance_v2.htc.id}"
-  depends_on = ["openstack_networking_router_interface_v2.htc"]
+  floating_ip   = "${openstack_networking_floatingip_v2.htc.address}"
+  instance_id   = "${openstack_compute_instance_v2.htc.id}"
+  depends_on    = ["openstack_networking_router_interface_v2.htc"]
 }
 
+// Security Groups
+resource "openstack_compute_secgroup_v2" "secgroup_htc_1" {
+  name          = "htc-priv-sec-grp"
+  description   = "htc-security group for private network"
+  rule {
+    from_port   = 0
+    to_port     = 65535
+    ip_protocol = "tcp"
+    cidr        = "10.0.0.0/24"
+  }
+}
+resource "openstack_compute_secgroup_v2" "secgroup_htc_2" {
+  name          = "htc-public-sec-grp"
+  description   = "htc-security group for public network"
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "137.158.0.0/16"
+  }
+}
+
+
+// Compute Instances
 // Create the headnode instance so that users are able to login and submit jobs.
 // The depends_on module is needed so that the subnet is available before the instance
 // is booted.
-
 resource "openstack_compute_instance_v2" "htc" {
   name              = "htc-headnode"
   availability_zone = "uct"
   image_id          = "${data.openstack_images_image_v2.ubuntu1604.id}"
   flavor_name       = "${var.flavor}"
   key_pair          = "${openstack_compute_keypair_v2.htc.name}"
-  security_groups    = ["default"]
-  depends_on = ["openstack_networking_subnet_v2.htc"]
+  security_groups   = ["${var.htc-public-sec-grp}","${var.htc-priv-sec-grp}"]
+  depends_on        = ["openstack_networking_subnet_v2.htc"]
   network {
     name = "htc"
   }
@@ -81,23 +104,22 @@ resource "openstack_compute_instance_v2" "htc" {
 }
 
 # Create the worker nodes and increase / decrease the count based on the number workers required
-
 resource "openstack_compute_instance_v2" "workers" {
-  count = 2
-  name = "${format("htc-worker-%02d", count.index+1)}"
-  key_pair = "${openstack_compute_keypair_v2.htc.name}"
+  count             = 2
+  name              = "${format("htc-worker-%02d", count.index+1)}"
+  key_pair          = "${openstack_compute_keypair_v2.htc.name}"
   availability_zone = "uct"
-  image_id = "${data.openstack_images_image_v2.ubuntu1604.id}"
-  flavor_name = "${var.flavor}"
-  security_groups = ["default"]
-  depends_on = ["openstack_compute_instance_v2.htc"]
+  image_id          = "${data.openstack_images_image_v2.ubuntu1604.id}"
+  flavor_name       = "${var.flavor}"
+  security_groups   = ["${var.htc-priv-sec-grp}"]
+  depends_on        = ["openstack_compute_instance_v2.htc"]
   network {
-    name = "htc"
+    name            = "htc"
   }
 }
 
 // The output is important so that Russ knows which headnode IP/DNS to connect to
 // once the terraform completes. Complete this please....
 output "ip" {
-  value = "${openstack_networking_floatingip_v2.htc.address}"
+  value             = "${openstack_networking_floatingip_v2.htc.address}"
 }
